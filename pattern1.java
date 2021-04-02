74973c2068a79ea0598e7873d515d3225401c85c
MD Arif Uddin <78405318+mdarifuddin1@users.noreply.github.com>
2021-04-02 10:46:07 +0600

    Add files via upload
    Pattern -1 program use java program

---
+++ pattern1.java
@@ -0,0 +1,26 @@
+
+package code;
+
+import java.util.Scanner;
+
+
+public class pattern1 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+        
+        for(int row= 1; row<=num; row++){
+        
+            for(int col=1; col<=row; col++){
+                System.out.print(" "+col);
+            }
+            
+            System.out.println(" ");
+    }
+    }
+    
+}
